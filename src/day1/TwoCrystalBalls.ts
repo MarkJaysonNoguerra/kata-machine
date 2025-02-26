35f12926ac0c793e3aad5cf27d631a48dc45eed4
mark <noguerra9999@gmail.com>
2025-02-27 06:27:55 +0800

    Implement the following algorithm * BinarySearchList * LinearSearchList * TwoCrystalBalls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,19 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+    const jump = Math.sqrt(breaks.length);
+
+    let i = jump;
+    for (; i < breaks.length; i += jump) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    for (let j = i - jump; j < i && j < breaks.length; j++) {
+        if (breaks[j]) {
+            return j;
+        }
+    }
+
+    return -1;
 }
