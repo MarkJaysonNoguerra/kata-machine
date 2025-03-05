af10bbc523c4c1ca558080ca0a5343dc2b0079ea
mark <noguerra9999@gmail.com>
2025-03-06 06:53:56 +0800

    Implement minHeap but need to redo it tomorrow to reinforce learning

--- src/day1/MinHeap.ts
+++ src/day1/MinHeap.ts
@@ -1,15 +1,83 @@
 export default class MinHeap {
     public length: number;
 
-    
+    public data: number[];
 
     constructor() {
+        this.length = 0;
+        this.data = [];
     }
 
     insert(value: number): void {
+        this.length++;
+        this.data.push(value);
+        this.heapifyUp();
+    }
 
-}
     delete(): number {
+        this.length--;
+        const result = this.data.shift();
+        this.heapifyDown();
 
-}
+        return result!;
+    }
+
+
+    private heapifyUp() {
+        let currentIndex = this.length - 1;
+        let parentIndex = Math.floor((currentIndex - 1) / 2);
+
+        while (parentIndex >= 0) {
+            if (this.data[parentIndex] > this.data[currentIndex]) {
+                this.swap(parentIndex, currentIndex);
+                currentIndex = parentIndex;
+                parentIndex = Math.floor((currentIndex - 1) / 2);
+                continue;
+            }
+
+            break;
+        }
+    }
+
+    private heapifyDown() {
+        let currentIndex = 0;
+        let leftIndex = (currentIndex * 2) + 1;
+        let rightIndex = (currentIndex * 2) + 2;
+
+        while (currentIndex < this.length) {
+            const leftValue = this.data[leftIndex];
+            const rightValue = this.data[rightIndex];
+            const currentValue = this.data[currentIndex];
+
+            if (leftValue < rightValue) {
+                if (currentValue > leftValue) {
+                    this.swap(leftIndex, currentIndex);
+                    currentIndex = leftIndex;
+                    leftIndex = (currentIndex * 2) + 1;
+                    rightIndex = (currentIndex * 2) + 2;
+                    continue;
+                }
+
+                break;
+            } else {
+                if (currentValue > rightValue) {
+                    this.swap(rightIndex, currentIndex);
+                    currentIndex = rightIndex;
+                    leftIndex = (currentIndex * 2) + 1;
+                    rightIndex = (currentIndex * 2) + 2;
+                    continue;
+                }
+
+                break;
+            }
+
+        }
+    }
+
+    private swap(a: number, b: number): void {
+        const temp = this.data[a];
+        this.data[a] = this.data[b];
+        this.data[b] = temp;
+    }
+
 }
