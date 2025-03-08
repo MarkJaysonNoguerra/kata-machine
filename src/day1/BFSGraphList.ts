40bad2563af421ef3e50a716fd0260f59deeef48
mark <noguerra9999@gmail.com>
2025-03-09 07:15:13 +0800

    Chore: Remvoe implementation for BFSGraphMatrix, BFSGraphList, and DFSGraphtList should be reimplemented next time before moving forward to reinforce learning

--- src/day1/BFSGraphList.ts
+++ src/day1/BFSGraphList.ts
@@ -1,26 +1,5 @@
 
 
 export default function bfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
-    const seen = Array.from({ length: graph.length }).fill(false) as boolean[];
-    const queue: Array<{ path: number[], node: number }> = [{ path: [source], node: source }];
 
-    while (queue.length) {
-        const { path, node } = queue.shift()!;
-        seen[node] = true;
-
-        const list = graph[node];
-        for (const item of list) {
-            if (seen[item.to]) {
-                continue;
-            }
-
-            if (item.to === needle) {
-                return path.concat(needle);
-            }
-
-            queue.push({ path: path.concat(item.to), node: item.to });
-        }
-    }
-
-    return null;
 }
--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,39 +1,3 @@
 export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
 
-    const seen = Array.from({ length: graph.length }).fill(false) as boolean[];
-    const previousPaths = Array.from({ length: graph.length }).fill(-1) as number[];
-    const queue = [source];
-
-    while (queue.length) {
-        const node = queue.shift()!;
-        seen[node] = true;
-
-        const adjacency = graph[node];
-        for (let i = 0; i < adjacency.length; i++) {
-            if (adjacency[i] === 0 || seen[i]) {
-                continue;
-            }
-
-            previousPaths[i] = node;
-            if (needle === i) {
-                break;
-            }
-
-            queue.push(i);
-        }
-    }
-
-    if (previousPaths[needle] === -1) {
-        return null;
-    }
-
-    let curretNode = previousPaths[needle];
-    const path = [needle];
-
-    while (curretNode >= 0) {
-        path.push(curretNode);
-        curretNode = previousPaths[curretNode];
-    }
-
-    return path.reverse();
 }
