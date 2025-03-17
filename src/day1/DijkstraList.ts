fa22f43464e244be352df644f8b52dfa6fac91b1
mark <noguerra9999@gmail.com>
2025-03-18 06:04:56 +0800

    Feat: Implement DijstraList and LRU

--- src/day1/DijkstraList.ts
+++ src/day1/DijkstraList.ts
@@ -1,3 +1,51 @@
+
+const getLowestUnvisited = (seen: boolean[], distances: number[]): number => {
+    let idx = -1;
+    let lowestWeight = Infinity;
+
+    for (let i = 0; i < seen.length; i++) {
+        if (!seen[i] && distances[i] < lowestWeight) {
+            idx = i;
+            lowestWeight = distances[i];
+        }
+    }
+
+    return idx;
+}
+
 export default function dijkstra_list(source: number, needle: number, graph: WeightedAdjacencyList): number[] {
 
+    const previous = Array.from({ length: graph.length }).fill(-1) as number[];
+    const seen = Array.from({ length: graph.length }).fill(false) as boolean[];
+    const distances = Array.from({ length: graph.length }).fill(Infinity) as number[];
+    distances[source] = 0;
+
+    while (seen.some(x => !x)) {
+        const node = getLowestUnvisited(seen, distances);
+        seen[node] = true;
+
+        const adjecency = graph[node];
+        for (const item of adjecency) {
+            if (seen[item.to]) {
+                continue;
+            }
+
+            const distance = distances[node] + item.weight;
+            if (distance < distances[item.to]) {
+                previous[item.to] = node;
+                distances[item.to] = distance;
+            }
+        }
+    }
+
+
+    let curr = previous[needle];
+    let output = [needle];
+    while (curr !== -1) {
+        output.push(curr);
+        curr = previous[curr];
+    }
+
+    output = output.reverse();
+    return output[0] === source ? output : [];
 }
--- src/day1/LRU.ts
+++ src/day1/LRU.ts
@@ -1,12 +1,104 @@
+type Node<T> = {
+    value: T;
+    previous?: Node<T>;
+    next?: Node<T>
+}
+
+const createNode = <T>(value: T): Node<T> => {
+    return {
+        value
+    }
+}
+
 export default class LRU<K, V> {
 
-    constructor() {
+    private lookUp: Map<K, Node<V>>;
+
+    private reverseLookUp: Map<Node<V>, K>;
+
+    private head?: Node<V>;
+
+    private tail?: Node<V>;
+
+    private length: number;
+
+    constructor(private capacity: number = 10) {
+        this.length = 0;
+        this.head = this.tail = undefined;
+        this.lookUp = new Map();
+        this.reverseLookUp = new Map();
     }
 
     update(key: K, value: V): void {
+
+        if (this.lookUp.has(key)) {
+            const node = this.lookUp.get(key)!;
+            this.detachNode(node);
+            this.prependNode(node);
+            node.value = value;
+            return;
+        }
+
+        this.length++;
+        const node = createNode(value);
+        this.lookUp.set(key, node);
+        this.reverseLookUp.set(node, key);
+        this.prependNode(node);
+        this.trimNode();
     }
 
 
     get(key: K): V | undefined {
+        const node = this.lookUp.get(key);
+        if (node) {
+            this.detachNode(node);
+            this.prependNode(node);
+            return node.value;
+        }
+
+        return undefined;
+    }
+
+    private detachNode(node: Node<V>): void {
+        if (node.previous) {
+            node.previous.next = node.next;
+        }
+
+        if (node.next) {
+            node.next.previous = node.previous
+        }
+
+        if (this.tail === node) {
+            this.tail = node.previous
+        }
+
+        if (this.head === node) {
+            this.head = node.next;
+        }
+
+        node.next = undefined;
+        node.previous = undefined;
+    }
+
+    private prependNode(node: Node<V>): void {
+        if (!this.head) {
+            this.head = this.tail = node;
+        }
+
+        node.next = this.head;
+        this.head.previous = node;
+        this.head = node;
+    }
+
+    private trimNode(): void {
+        if (this.length > this.capacity) {
+            this.length--;
+            const node = this.tail!;
+            const key = this.reverseLookUp.get(node)!;
+            this.lookUp.delete(key);
+            this.reverseLookUp.delete(node);
+            this.detachNode(this.tail!);
+        }
+
     }
 }
